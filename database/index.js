5087b3013c45c474f3f227837f2b1eb6b9bce7ec
ibnusyawall <website.isas.24@gmail.com>
2021-04-03 04:44:35 +0000

    ✨ push all project

--- database/index.js
+++ database/index.js
@@ -4,6 +4,4 @@ DB = new Nedb({ filename: process.cwd() + '/database/db.json' })
 
 DB.loadDatabase(e => e ? console.log(e) : console.log('[√] success db loaded') )
 
-//DB.insert({ ip: '127.0.0.1', location: '' }, (e, res) => console.log(res))
-DB.find({}, (e, res) => console.log(res))
 module.exports = DB
